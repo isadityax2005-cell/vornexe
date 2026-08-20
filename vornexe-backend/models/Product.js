ccb8e77b87d86a165228dbaa49713d768321cb9e
Aditya Singh <isadityax2005@gmail.com>
2026-08-21 03:22:18 +0530

    Fix API response to use id instead of _id

--- vornexe-backend/models/Product.js
+++ vornexe-backend/models/Product.js
@@ -10,4 +10,13 @@ const productSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
+productSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+  }
+});
+
 module.exports = mongoose.model('Product', productSchema);
